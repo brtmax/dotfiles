36dc4655b7ed747a2a49f01958291154e8dc0127
Max Bretschneider <maxbretschneider@mailbox.org>
2023-06-20 15:23:17 +0200

    Added config for neorg

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,13 +21,25 @@ local default_plugins = {
 
   {
     "nvim-neorg/neorg",
-    run = ":Neorg sync-parsers",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("neorg").setup {
-        -- configuration here
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
       }
     end,
   },
+
 
   {
     "NvChad/ui",
