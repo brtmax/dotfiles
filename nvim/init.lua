6e92592f821f8fdacd7c997dc3b2825973ee26d7
Max Bretschneider <maxbretschneider@mailbox.org>
2024-11-22 14:45:22 +0100

    Markdown nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -325,6 +325,16 @@ require("lazy").setup({
     },
   },
 
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
