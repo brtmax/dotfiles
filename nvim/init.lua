9318719d931f3054f39fb25cc43b3ff3ef5d1de5
Max Bretschneider <maxbretschneider@mailbox.org>
2024-12-02 11:16:35 +0100

    nvim dap

--- nvim/init.lua
+++ nvim/init.lua
@@ -256,6 +256,8 @@ require("lazy").setup({
     },
   },
 
+  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
