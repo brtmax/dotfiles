ab89d5e9234bc94851ce6d1faa4df2c6ce841fac
mabrets <max.bretschneider@mercedes-benz.com>
2024-12-14 17:38:31 +0100

    Removed unused scripts

---
+++ setup.sh
@@ -0,0 +1,14 @@
+clone dotfiles
+clone engineering-daybook
+
+install zsh and oh my zsh
+
+ln nvim, tmux, zsh
+
+zsh clone autosuggestions
+zsh clone syntax highlighting
+zsh clone pixegami-agnoster theme
+
+ctrl swap 
+
+
