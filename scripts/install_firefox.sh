175552ea0bd3f31ffcf3a86a8ecf5c2f9645564f
mabrets <max.bretschneider@mercedes-benz.com>
2024-12-15 16:56:57 +0100

    Script to setup firefox

---
+++ scripts/install_firefox.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+echo -n "Enter name of the firefox profile: " && read name
+addonlist="ublock-origin,
+tridactyl-vim,
+darkreader,
+istilldontcareaboutcookies,
+sidebery,
+popupoff,
+nicothin-space,
+sponsorblock,
+windscribe,
+violentmonkey"
+
+echo "Creating Profile"
+firefox -CreateProfile $name
+folder=$(sed -n "/Path=.*.$name$/ s/.*=//p" ~/.mozilla/firefox/profiles.ini)
+path="/home/$(whoami)/.mozilla/firefox/$folder"
+cd $path
+echo "Profile Creation Finished"
+
+echo "Downloading Addons"
+addontmp="$(mktemp -d)"
+trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
+mozillaurl="https://addons.mozilla.org"
+IFS=$'\n,'
+mkdir -p "$path/extensions/"
+for addon in $addonlist; do
+	echo "Installing $addon"
+	addonurl="$(curl --silent "$mozillaurl/en-US/firefox/addon/${addon}/" | grep -o "$mozillaurl/firefox/downloads/file/[^\"]*")"
+	file="${addonurl##*/}"
+	curl -LOs "$addonurl" >"$addontmp/$file"
+	id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
+	id="${id%\"*}"
+	id="${id##*\"}"
+	mv "$file" "$path/extensions/$id.xpi"
+done
+echo "Addons Installed"
